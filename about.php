7e57b1a79dffc93f54616771b13e42ef130a3d22
ricoferdinandd <rico.ferdinand@student.umn.ac.id>
2021-11-23 20:12:15 +0700

    SistemPakar

---
+++ about.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Sistem Pakar</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<div class="container-fluid text-center">    
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      <p><a href="index.php"><button type="button" class="btn btn-primary btn-block">BERANDA</button></a></p>
+      <p><a href="diagnosa.php"><button type="button" class="btn btn-primary btn-block">DIAGNOSA PENYAKIT</button></a></p>
+      <p><a href="daftarpenyakit.php"><button type="button" class="btn btn-primary btn-block">DAFTAR PENYAKIT</button></a></p>
+      <p><a href="about.php"><button type="button" class="btn btn-primary btn-block active">ABOUT</button></a></p>
+        <br><br><br><br><br><br><br><br><br><br>
+      <p><button type="button" class="btn btn-primary btn-block" id="myBtn">LOGIN</button></p>
+    </div>
+    <div class="col-sm-8 text-left"> 
+      <center><h2>ABOUT</h2></center>
+      <div class="panel panel-success">
+      <div class="panel-heading">Meet our Team</div>
+      <div class="panel-body">
+      <div class="row">
+        <div class="column" style="float: left; width: 33.3%; margin-bottom: 16px; padding: 0 8px">
+          <div class="card" style ="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);">
+            <img src="moren.jpg" alt="Maureen" style="width:100%">
+            <div class="container" style="padding: 0 16px;">
+              <h2>Maureen Zerlina</h2>
+              <p class="title" style="color: grey; font-size: 18px">Editor</p>
+              <p style="font-size: 18px">maureen.zerlina@student.umn.ac.id</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column" style="float: left; width: 33.3%; margin-bottom: 16px; padding: 0 8px">
+        <div class="card" style ="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);">
+            <img src="rico.jpg" alt="Rico" style="width:100%">
+            <div class="container" style="padding: 0 16px;">
+              <h2>Rico Ferdinand</h2>
+              <p class="title" style="color: grey; font-size: 18px">Web Developer</p>
+              <p style="font-size: 18px">rico.ferdinand@student.umn.ac.id</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column" style="float: left; width: 33.3%; margin-bottom: 16px; padding: 0 8px">
+        <div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);">
+            <img src="ze.jpg" alt="Zefanya" style="width:100%">
+            <div class="container" style="padding: 0 16px;">
+              <h2>Zefanya Natanael</h2>
+              <p class="title" style="color: grey; font-size: 18px">Observator</p>
+              <p style="font-size: 18px">zefanya1@student.umn.ac.id</p>
+            </div>
+          </div>
+        </div>
+      </div>
+          </div>
+    </div>
+    </div>
+  </div>
+</div>
+ <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header" style="padding:35px 50px;">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4><span class="glyphicon glyphicon-lock"></span> Login</h4>
+        </div>
+        <div class="modal-body" style="padding:40px 50px;">
+         <form role="form" method="post" action="ceklogin.php">
+            <div class="form-group" method="post">
+              <label for="username"><span class="glyphicon glyphicon-user"></span> Username</label>
+              <input type="text" class="form-control" name="username" id="password" placeholder="Enter username">
+            </div>
+            <div class="form-group" method="post">
+              <label for="password"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+              <input type="password" class="form-control" name="password" id="password" placeholder="Enter password">
+            </div>
+              <button type="submit" id="submit" nama="submit" class="btn btn-primary btn-block" method="post"><span class="glyphicon glyphicon-off"></span> Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div> 
+<footer class="container-fluid text-center">
+  <p>Teknik Informatika 2019</p>
+</footer>
+<script>
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+});
+</script>
+
+</body>
+</html>
